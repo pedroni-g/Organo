87e8f348dccdc1cd76e58b58595217888d1a1b10
Gabriel Pedroni <sleepy.pepperoni@gmail.com>
2024-11-11 17:31:05 -0300

    Adicionados novos componentes no formulario

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,16 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+    const placeholderModificado = `${props.placeholder}...`
+
+    return (
+        <div className="campo__texto">
+            <label>
+                {props.label}
+            </label>
+            <input placeholder={placeholderModificado}/>
+        </div>
+    )
+}
+
+export default CampoTexto
