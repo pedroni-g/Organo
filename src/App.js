40d81e41451c707bb30449f65d491b0dab8cc44b
Gabriel Pedroni <sleepy.pepperoni@gmail.com>
2024-11-27 10:39:02 -0300

    ultimo ajuste do curso

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
-
+import Rodape from './componentes/Rodape';
 function App() {
 
 	const times = [
@@ -45,7 +45,7 @@ function App() {
 
 	const [colaboradores, setColaboradores] = useState([])
 	const aoNovoColaboradorAdicionado = (colaborador) => {
-		console.log(colaborador)
+		// debugger
 		setColaboradores([...colaboradores, colaborador])
 	}
 
@@ -64,6 +64,7 @@ function App() {
 				colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
 			/>
 			)}
+			<Rodape />
 		</div>
 	);
 }
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -5,10 +5,11 @@ const ListaSuspensa = (props) => {
         <div className='lista__suspensa'>
             <label>{props.label}</label>
             <select
-                value={props.value}
+                value={props.valor}
                 onChange={evento => props.aoAlterado(evento.target.value)}
                 required={props.obrigatorio}
             >
+            <option value=""></option>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
